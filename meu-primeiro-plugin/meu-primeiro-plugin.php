b186449ae48f7c84618c006d99800dd430991b6f
Gustavo Palmeira <gustavo_palmeira07@hotmail.com>
2020-10-27 20:45:47 -0300

    Plugin Open Graph Tag

--- meu-primeiro-plugin/meu-primeiro-plugin.php
+++ meu-primeiro-plugin/meu-primeiro-plugin.php
@@ -17,3 +17,24 @@ function altera_msg_login()
 {
   return 'Credenciais inválidas';
 }
+
+add_action('wp_head', 'colocar_og_tags');
+
+function colocar_og_tags()
+{
+
+  $dados_do_post = get_post(get_the_ID());
+  $dados_do_site = get_bloginfo();
+
+  $titulo = $dados_do_post->post_title;
+  $resumo = $dados_do_post->post_excerpt;
+
+  if (is_single()) {
+    echo "    \n\n\n
+              <meta property='og:title' content='" . $titulo . "'>
+              <meta property='og:title_name' content='" . $dados_do_site . "'>
+              <meta property='og:title_url' content='" . get_permalink() . "'>
+              <meta property='og:title_description' content='" . $resumo . "'>
+              \n\n\n";
+  };
+};
