6e9559f17285c42d82ecefbdf9cfb746c9bf284e
Gustavo Palmeira <gustavo_palmeira07@hotmail.com>
2020-11-17 20:46:38 -0200

    Início CRUD, listar, gravar e deletar

--- crud-plugin/crud-plugin.php
+++ crud-plugin/crud-plugin.php
@@ -13,8 +13,9 @@ Lisence: CC BY
 
 if (!defined('WPINC')) exit;
 
-register_activation_hook(__FILE__, 'criar_tabela');
 
+// Ativação do Plugin
+register_activation_hook(__FILE__, 'criar_tabela');
 function criar_tabela()
 {
 
@@ -24,4 +25,62 @@ function criar_tabela()
                    (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
                     nome varchar(255) NOT NULL,
                     whatsapp BIGINT UNSIGNED NOT NULL)");
+}
+
+// Desativação do Plugin
+register_deactivation_hook(__FILE__, 'apagar_tabela');
+function apagar_tabela()
+{
+
+  global $wpdb;
+
+  $wpdb->query("DROP TABLE {$wpdb->prefix}agenda");
+}
+
+// Criando página 
+add_action('admin_menu', 'crud_menu');
+function crud_menu()
+{
+  add_menu_page(
+    'Configurações do CRUD',
+    'CRUD',
+    'administrator',
+    'Meu-plugin-config',
+    'abre_configuracoes',
+    'dashicons-database'
+  );
+}
+
+function abre_configuracoes()
+{
+
+  global $wpdb;
+
+  // DELETAR
+  if (isset($_GET['apagar'])) {
+
+    $id = preg_replace('/\D/', '', $_GET['apagar']);
+
+    $wpdb->query("DELETE FROM {$wpdb->prefix}agenda WHERE id = $id");
+  }
+
+
+  // GRAVAR
+  if (isset($_POST['submit'])) {
+    if (isset($_POST['submit']) == 'Gravar') {
+
+      $wpdb->query($wpdb->prepare(
+        "INSERT INTO {$wpdb->prefix}agenda
+                                        (nome, whatsapp) 
+                                      VALUES
+                                        ( %s, %d)",
+        $_POST['nome'],
+        $_POST['whatsapp']
+      ));
+    }
+  }
+
+  $contatos = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}agenda");
+
+  require 'lista_tpl.php';
 }
---
+++ crud-plugin/lista_tpl.php
@@ -0,0 +1,34 @@
+<div class="wrap">
+  <h1>Configurações do CRUD</h1>
+  <br><br>
+
+  <center>
+    <form action="" method="post">
+      <table width="50%" style="text-align: center">
+        <tr>
+          <td style="font-size: 25px;">Nome</td>
+          <td style="font-size: 25px;">Whatsapp</td>
+          <td></td>
+        </tr>
+
+        <?php
+        foreach ($contatos as $value) {
+          echo "<tr>
+                  <td><br> {$value->nome} </td>
+                  <td><br> {$value->whatsapp} </td>
+                  <td><br> <a href='?page={$_GET['page']}&apagar={$value->id}'> Apagar </a></td>
+                </tr>";
+        }
+        ?>
+
+        <tr>
+          <td><input type="text" name="nome"> </td>
+          <td><input type="text" name="whatsapp"> </td>
+          <td><?php submit_button('Gravar'); ?> </td>
+        </tr>
+
+      </table>
+    </form>
+  </center>
+
+</div>
